522674edeea5bc55731fbb6498295c30c5a54dea
shizu-p <nukuzihs@gmail.com>
2023-11-02 20:42:14 +0900

    added eratothenes

---
+++ eratosthenes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,a) for(int i = 0; i < (a); ++i)
+
+template<typename T> bool chmax(T &a,T b){if(a<b){a=b; return true;} return false;}
+template<typename T> bool chmin(T &a,T b){if(a>b){a=b; return true;} return false;}
+
+vector<int> eratos(int n){
+    vector<bool> isprime(n+1,true);
+    vector<int> prime;
+    for(int i=2;i<n;++i){
+        if(!isprime[i]) continue;
+        prime.push_back(i);
+        for(int j=i*2;j<=n;j+=i) isprime[j] = false;
+    }
+    return prime;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto p = eratos(1000);
+    for(auto s : p) cout << s << endl;
+}
