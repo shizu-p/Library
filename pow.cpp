083e81821035096142d9441628f3074275f961cb
shizu <112470594+shizu-p@users.noreply.github.com>
2023-11-05 15:36:44 +0900

    a little changes

--- pow.cpp
+++ pow.cpp
@@ -1,16 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-#define rep(i,a) for(int i = 0; i < (a); ++i)
-
-template<typename T> bool chmax(T &a,T b){if(a<b){a=b; return true;} return false;}
-template<typename T> bool chmin(T &a,T b){if(a>b){a=b; return true;} return false;}
 
 struct pow_ll{
-    ll a,x;
-    pow_ll(ll a,ll x):a(a),x(x){}
-    ll res = 1;
-    ll pow(){
+    long long a,x;
+    pow_ll(long long a,long long x):a(a),x(x){}
+    long long res = 1;
+    long long pow(){
         while(x>0){
             if(x&1) res*=a;
             a*=a;
